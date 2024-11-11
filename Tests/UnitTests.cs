f356e0db63df8539c174e9818df6853e53307aaf
GMIELAK <147776706+GMIELAK@users.noreply.github.com>
2024-11-10 21:40:35 -0600

    Failing Test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,15 +7,15 @@ namespace GithubActionsLab
     public class Math
     {
         [Test]
-        public void Add_Valid()
+        public void Add_ValidGrantMielak()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
-            Assert.AreEqual(5, Program.Add("3", "2"));
+            Assert.AreEqual(6, Program.Add("3", "2"));
             Assert.AreEqual(12, Program.Add("5", "7"));
         }
 
         [Test]
-        public void Add_Invalid()
+        public void Add_InvalidGrantMielak()
         {
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
@@ -23,13 +23,114 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Null()
+        public void Add_NullGrantMielak()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
 
-        // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_ValidGrantMielak()
+        {
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(-4, Program.Subtract("3", "7"));
+            Assert.AreEqual(0, Program.Subtract("5", "5"));
+        }
+
+        [Test]
+        public void Subtract_InvalidGrantMielak()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_NullGrantMielak()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Multiply_ValidGrantMielak()
+        {
+            Assert.AreEqual(6, Program.Multiply("3", "2"));
+            Assert.AreEqual(0, Program.Multiply("0", "7"));
+            Assert.AreEqual(-15, Program.Multiply("3", "-5"));
+        }
+
+        [Test]
+        public void Multiply_InvalidGrantMielak()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_NullGrantMielak()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        [Test]
+        public void Divide_ValidGrantMielak()
+        {
+            Assert.AreEqual(2, Program.Divide("4", "2"));
+            Assert.AreEqual(0, Program.Divide("0", "7"));
+            Assert.AreEqual(-3, Program.Divide("9", "-3"));
+        }
+
+        [Test]
+        public void Divide_InvalidGrantMielak()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Divide_NullGrantMielak()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
+        [Test]
+        public void Divide_ByZeroGrantMielak()
+        {
+            Assert.Throws<DivideByZeroException>(() => Program.Divide("1", "0"));
+        }
+
+        [Test]
+        public void Power_ValidGrantMielak()
+        {
+            Assert.AreEqual(8, Program.Power("2", "3"));
+            Assert.AreEqual(1, Program.Power("5", "0"));
+            Assert.AreEqual(0.25, Program.Power("2", "-2"), 0.0001);
+        }
+
+        [Test]
+        public void Power_InvalidGrantMielak()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        [Test]
+        public void Power_NullGrantMielak()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
     }
 }
+
